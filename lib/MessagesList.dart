23b1b4446aa2f83a7863e81a136fb0bfca686797
El Hadji Koky Dramé <5286846-thiapathioli@users.noreply.gitlab.com>
2020-04-28 05:19:44 +0200

    second commit

---
+++ lib/MessagesList.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:emailapp/Message.dart';
+
+
+class MessageList extends StatefulWidget {
+final String title;
+
+  const MessageList({Key key, this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MessageListState();
+}
+
+class _MessageListState extends State<MessageList> {
+var messages = const [];
+
+Future loadMessageList() async{
+  String content = await rootBundle.loadString("data/message.json");
+  List collection = json.decode(content);
+  List<Message> _messages = collection.map((json) => Message.fromJson(json)).toList();
+
+  setState(() {
+    messages = _messages;
+  });
+}
+
+void initState(){
+  loadMessageList();
+  super.initState();
+}
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.separated(
+        itemCount: messages.length,
+        separatorBuilder: (context, index) => Divider(),
+        itemBuilder: (BuildContext context, int index){
+          Message message = messages[index];
+          return ListTile(
+            title: Text(message.subject),
+            isThreeLine: false,
+            leading: CircleAvatar(
+              child: Text("KD"),
+              ),
+              subtitle: Text(
+                message.body,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+          );
+        }
+      ),
+    );
+  }
+}
